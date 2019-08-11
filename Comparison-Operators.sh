4d1f9b7ff12116e67404f25019fc56da80aeca36
janethavi <janethavishka@gmail.com>
2019-08-11 12:54:05 +0530

    If condition with gt

--- Comparison-Operators.sh
+++ Comparison-Operators.sh
@@ -3,8 +3,16 @@
 var1=10
 var2=20
 
+# if $var1 is equal to $var2
 if [ $var1 -eq $var2 ]; then
     echo "var1 is equals to var2"
 else
     echo "var1 is not equal to var2"
+fi
+
+# if $var1 is equal to $var2
+if [ $var1 -gt $var2 ]; then
+    echo "var1 is greater than var2"
+else
+    echo "var1 is less than var2"
 fi
